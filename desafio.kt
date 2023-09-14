60308cdb850ff5db183bfe56e226cf287a202d0f
Gabriela Giubine <80356797+Giubine@users.noreply.github.com>
2023-09-14 17:51:33 -0300

    Update desafio.kt
    Desafio Dio Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel = calcularNivel(duracao))
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+    
 }
+    
+    fun calcularNivel(duracao: Int): Nivel {
+    return when {
+        duracao <= 60 -> Nivel.BASICO
+        duracao <= 120 -> Nivel.INTERMEDIARIO
+        else -> Nivel.DIFICIL
+    }
+    }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90) 
+    val conteudo2 = ConteudoEducacional("Banco de Dados SQL", 120)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Web", 150)
+
+    val formacao1 = Formacao("Desenvolvedor Web Full-Stack", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao2 = Formacao("Analista de Dados", listOf(conteudo2))
+
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao2.matricular(usuario1)
+    
+    formacao1.conteudos.forEach { conteudo ->
+        println("${conteudo.nome} - Nível: ${conteudo.nivel}")
+    }
+    formacao2.conteudos.forEach { conteudo ->
+        println("${conteudo.nome} - Nível: ${conteudo.nivel}")
+    }
+
+    println("Inscritos na Formação 1: ${formacao1.inscritos}, nível")
+    println("Inscritos na Formação 2: ${formacao2.inscritos}, nível")
 }
